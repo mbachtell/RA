8d57a366ecdb81b050803f0a0b1319562152ffbd
mbachtell <mbachtell@users.noreply.github.com>
2014-04-26 09:52:15 -0400

    Create toggle.js

---
+++ javascript/toggle.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+     // hides the slickbox as soon as the DOM is ready
+
+      $('#slickbox').hide();
+     // shows the slickbox on clicking the noted link  
+     
+		 // toggles the slickbox on clicking the noted link  
+      $('#slick-toggle').click(function() {
+        $('#slickbox').toggle(400);
+        return false;
+      });
+
+      $('#slickbox2').hide();
+     // shows the slickbox on clicking the noted link  
+     
+		 // toggles the slickbox on clicking the noted link  
+      $('#slick-toggle2').click(function() {
+        $('#slickbox2').toggle(400);
+        return false;
+      });
+
+      $('#slickbox3').hide();
+     // shows the slickbox on clicking the noted link  
+     
+		 // toggles the slickbox on clicking the noted link  
+      $('#slick-toggle3').click(function() {
+        $('#slickbox3').toggle(400);
+        return false;
+      });			
+			
+    });
